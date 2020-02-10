bc7596269dc380bb40eea8cf63c903df46ff5da2
Rahul <rahulk1.aipl@gmail.com>
2020-02-10 18:03:29 +0530

    Updated web.php file for routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -50,28 +50,10 @@ Route::group(['middleware' => ['api']], function() {
         Route::get('logout', 'API\v1\UsersController@logout');
         Route::put('user', 'API\v1\UsersController@editUser');
         Route::get('user', 'API\v1\UsersController@getUsers');
-        Route::get('category', 'API\v1\ServicesController@index');
         Route::post('addReview', 'API\v1\BookingController@addReview');
-        Route::get('getServices', 'API\v1\ServicesController@get_all_services');
-        Route::get('doctor/{id}', 'API\v1\ServicesController@getUserById');
         Route::post('checkPhoneOtp', 'API\v1\UsersController@checkPhoneOtp');
-        Route::post('upload', 'API\v1\MediaController@uploadMedia');
-        Route::post('appointment', 'API\v1\BookingController@bookService');
-        Route::get('appointment', 'API\v1\BookingController@getAppointments');
-        Route::get('reports', 'API\v1\BookingController@reportList');
-        Route::get('slots', 'API\v1\venderSlotController@getSlots');
-        Route::post('card', 'API\v1\CardController@addCard');
-        Route::get('card', 'API\v1\CardController@getCard');
         Route::post('uploadImage', 'API\v1\UsersController@uploadImage');
-        Route::get('mydoctors', 'API\v1\ServicesController@mydoctors');
-        Route::get('faq', 'API\v1\FaqController@getFaq');
-        Route::get('coupons', 'API\v1\CouponController@getCoupons');
-        Route::post('validate_coupon', 'API\v1\CouponController@validateCoupon');
-        Route::get('notification', 'API\v1\NotificationController@getNotifications');
-        Route::patch('appointment', 'API\v1\BookingController@updateStatus');
-        Route::get('doctor_reports', 'API\v1\BookingController@doctorReports');
-        Route::get('doctor_reports_list', 'API\v1\BookingController@doctorReportsList');
-        Route::patch('notification', 'API\v1\NotificationController@updateStatus');
+        
     });
         
 });
