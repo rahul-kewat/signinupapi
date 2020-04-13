9e797bc3e8ddc3651812891146bf32be09b6cbce
Rahul Kewat <30612368+rahul-kewat@users.noreply.github.com>
2020-04-13 15:13:35 +0530

    Update User.php

--- src/Resources/User.php
+++ src/Resources/User.php
@@ -3,10 +3,15 @@
 namespace Devrahul\Signinupapi\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
-use App\UserAddresses;
+use Devrahul\Signinupapi\Models\UserAddresses;
 use App\Http\Resources\AddressCollection;
 use App\Http\Resources\VenderServiceCollection;
 use Illuminate\Support\Facades\Auth;
+use App\Models\RideAccepted;
+use App\Models\Rides;
+use DB;
+use App\Models\Transaction;
+use App\Setting;
 
 class User extends Resource {
 
@@ -22,7 +27,69 @@ class User extends Resource {
     private $is_bank_detail_added;
     private $referral_code_text;
     protected $address=array();
+    protected $is_pending_payment = array();
    
+    public function getPaymentDetail($user){
+        // check the ride owner of the ride_id is same as auth user
+        $resultQuery = Rides::where('id',$user->ride_id);
+        $data = $resultQuery->first();
+        $value =$user->ride_id;
+        $resultQuery = Rides::with([
+            'rideAccepted',
+        ])
+        ->whereIn('status',[0,1]);
+        $data = $resultQuery->get();            
+        $noofseats_accepted = 0;
+       
+        // If the query count is greater then 0 then it will proceed further
+        $noofseats_invehicle = $data[0]['max_no_seats'];
+        foreach($data[0]['rideAccepted'] as $data1){
+            $noofseats_accepted = $noofseats_accepted+ $data1["consume_seats"];
+        }
+        $availableseats = $noofseats_invehicle - $noofseats_accepted;
+        if(count($data) > 0){
+                if($availableseats>0)
+                {   
+                    //set available seats to 1 as only 1 seat is booked by the passenger
+                    $availableseats=1; // comment this line when you want to book more then one seats 
+
+                    //Counting the records of referral code of the user which are not used
+                    $records = DB::table("referral_record")->select('*')
+                                ->whereNotIn('code',function($query){
+                                                $query->select('referral_code')->from('transactions');
+                                })
+                                ->where(function ($query) use($user){
+                                    $query->where('user_id_two','=',$user->user_id)
+                                            ->orWhere('user_id_one','=',$user->user_id);
+                                })
+                                ->get();
+                    $inviteCodeDiscount=0;
+                    if(count($records)>0){
+                        $inviteCodeDiscount = Setting::pluck('referral_code_discount')->first();
+                    }
+                    $payableamount_withoutdiscount = $data[0]['price_per_seat']*$availableseats + $data[0]['total_toll_tax'];    
+                    $discountapplied = ($payableamount_withoutdiscount * $inviteCodeDiscount)/100;
+                    if(count($records)>0){
+                        $referral_Code_To_Use = $records[0]->code;
+                    }
+                    else{
+                        $referral_Code_To_Use = "";
+                    }
+                    
+                    $payableamount_withoutdiscount = number_format(round($payableamount_withoutdiscount,2),2);
+                    $discountapplied = number_format(round($discountapplied,2),2);
+                    $with_Discount_Payable =number_format(round($payableamount_withoutdiscount - $discountapplied,2),2);                               
+                }
+        }
+        $dataPush = [
+            'ride_id' => $user->ride_id,
+            'referral_code' => $referral_Code_To_Use ,
+            'original_amount' =>$payableamount_withoutdiscount,
+            'discount' => $discountapplied,
+            'amount' => $with_Discount_Payable,
+        ];
+        return $dataPush;
+    }
 
     public function __construct($resource,$token,$is_vehicle_added,$is_bank_detail_added,$sug_price_value,$referral_code_text) {
         parent::__construct($resource);
@@ -32,7 +99,11 @@ class User extends Resource {
         $this->is_bank_detail_added = $is_bank_detail_added;
         $this->referral_code_text = $referral_code_text;
         $this->address = UserAddresses::where('user_id',Auth::user()->id)->select("id","user_id","latitude","longitude","address_type","name","phone","country","city","pincode","full_address","house_no","landmark")->get();
-       
+        $user = RideAccepted::where(['ride_accepted.user_id'=> Auth::user()->id , 'ride_accepted.status' => 0, 'is_accepted_by_driver' => 1 ])->join('ride','ride.id','ride_id')->orderBy('ride_accepted.updated_at')->select('ride_accepted.ride_id','ride_accepted.user_id','ride_accepted.status','ride_accepted.is_accepted_by_driver','ride_accepted.updated_at','ride.id','ride.user_id','ride.status as rideStatus','ride.max_no_seats')->first();
+
+        $this->is_pending_payment = $this->getPaymentDetail($user);
+
+
     }
 
 
@@ -57,6 +128,7 @@ class User extends Resource {
             'is_bank_detail_added' => $this->is_bank_detail_added ,
             'referral_code_text' => $this->referral_code_text != null ? $this->referral_code_text : '',
             'address' => $this->address != null ? $this->address : '',
+            'payment_pending_ride' => $this->is_pending_payment != null ? $this->is_pending_payment : "",
         ];
     }
 }
